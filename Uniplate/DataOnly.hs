68f1f67a13cf19aa0f98b72fa466d3568852647c
Paulo Lieuthier <paulolieuthier@gmail.com>
2015-09-18 15:25:31 -0300

    Remove unnecessary Uniplate directory

--- Uniplate/DataOnly.hs
+++
@@ -1,6 +0,0 @@
-{-# LANGUAGE CPP, FlexibleInstances, FlexibleContexts, MultiParamTypeClasses #-}
-module Uniplate.DataOnly where
-import Data.Generics.Uniplate.DataOnly
-import Data.Generics.Uniplate.Data.Instances
-#define SKIP_ZIPPER
-#include "CommonInc.hs"
